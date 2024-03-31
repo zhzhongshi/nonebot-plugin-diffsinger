a33911350714a026794fa4dad69ec1b4c44e5dce
zhzhongshi <39423408+zhzhongshi@users.noreply.github.com>
2024-03-31 22:15:06 +0800

    rafactor

--- nonebot_plugin_diffsinger/__init__.py
+++ nonebot_plugin_diffsinger/__init__.py
@@ -1,109 +1,65 @@
-import base64
-import asyncio
-import httpx
-import json
-import requests
 from nonebot import on_command
 from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.adapters.onebot.v11.message import Message,MessageSegment
 from nonebot.adapters.onebot.v11.event import  MessageEvent
 from nonebot.params import CommandArg
-async def getWavId(jsonstr):
-    url="http://127.0.0.1:8000/api/infer/"
-    async with httpx.AsyncClient() as client:
-        data=jsonstr
-        resp = await client.post(url,data=data,timeout=280)
-        return resp
-def getWav(id):
-    url="http://127.0.0.1:8000/api/getwav/"
-    resp = requests.get(url=(url+"?uuid="+id))
-    return resp.content
-sing = on_command("ds", priority=7)
-sang = on_command("dstest", priority=8)
-@sing.handle()
-async def _(bot: Bot, event: MessageEvent, arg: Message=CommandArg()):
-    arg = arg.extract_plain_text()
-    print(arg)
-    await sing.send("开始合成")
-    resp=await getWavId(arg)
-    result=resp.json()
-    if resp.status_code != 200:
-        dat=result['error']
-        await sing.send("HTTP/1.1 "+str(resp.status_code)+"\n"+dat)
-    else:
-        id=result['id']
-        recmsg=MessageSegment.record(file=bytes(getWav(id)))
-        msg=Message(recmsg)
-        await sing.send(msg)
-    return 
 
-@sang.handle()
-async def _(bot: Bot, event: MessageEvent, arg: Message=CommandArg()):
-    arg = arg.extract_plain_text()
-    print(arg)
-    arg=note2data(arg)
-    await sing.send("开始合成")
-    resp=await getWavId(arg)
-    result=resp.json()
-    if resp.status_code != 200:
-        dat=result['error']
-        await sing.send("HTTP/1.1 "+str(resp.status_code)+"\n"+dat)
-    else:
-        id=result['id']
-        recmsg=MessageSegment.record(file=bytes(getWav(id)))
-        msg=Message(recmsg)
-        await sing.send(msg)
-    return 
-'''
-{"tempo":120,"data":
-["小酒窝长睫毛AP是你最美的记号",
-"C#4 | F#4| G#4 | A#4 F#4 | F#4 C#4 | C#4 | rest | C#4 | A#4 | G#4 | A#4 | G#4 | F4 | C#4",
-"2 |2 | 2 | 2 2 | 2 2 | 3 | 1 | 2 | 2 | 2 | 2 | 2 | 2 | 2"]
-}
-'''
-def note2data(arg: str):
-    print(arg)
-    # 参数处理
-    seq=json.loads(arg)
+from .data_source import get_f0, process_notes, bpm2dur
+
+import json
+import httpx
+import time
+ds = on_command("diffsinger", priority=7)
+url="http://127.0.0.1:9266/"
+
     
-    # 参数解析,计算时值
-    bpm=seq["tempo"]
-    lrcdata=seq["data"]
-    lyric=str(lrcdata[0])
-    note_per_word_lst = [x.strip() for x in str(lrcdata[1]).split('|') if x.strip() != '']
-    mididur_per_word_lst = [x.strip() for x in str(lrcdata[2]).split('|') if x.strip() != ''] # [1,1,2 2,2]
-    if not len(note_per_word_lst) == len(mididur_per_word_lst):
-        return "{\"error\":\"长度不一致\"}"
-    else: 
-        # 构造json
-        dur_per_word_lst=[]
-        for i in mididur_per_word_lst:
-            mididur_per_note=i.split(" ")
-            if len(mididur_per_note) >1:
-                midpnote=[] # [2 2]
-                for m in mididur_per_note:
-                    if not(m==""):
-                        # 16分音符
-                        j=float(m)
-                        # 该音符时长
-                        t = float ((60/int(bpm)/4)*j)
-                        # 保留7位小数
-                        t2=round(t,7)
-                        midpnote.append(str(t2))
-                midpword=str(" ".join(midpnote))
-                dur_per_word_lst.append(midpword)
-            else:
-                # 16分音符
-                j=float(i)
-                # 该音符时长
-                t = float ((60/int(bpm)/4)*j)
-                # 保留7位小数
-                t2=round(t,7)
-                dur_per_word_lst.append(str(t2))
-        durs=str("|".join(dur_per_word_lst))
-        notes=str("|".join(note_per_word_lst))
-        datas=[lyric,notes,durs]
-        data={"data":datas}
-        jsonobj=json.dumps(data)
-        print(jsonobj)
-        return jsonobj
+@ds.handle()
+async def sendcmd(bot: Bot, event: MessageEvent, arg: Message=CommandArg()):
+    arg = arg.extract_plain_text()
+    preps=[]
+    lists=str(arg).split("|")
+    for i in lists:
+        notes=i.split("~")
+        prep=bpm2dur(int(notes[0]),json.loads(notes[1]))
+        preps.extend(prep)
+    
+    phone_dict=httpx.request("GET",f"{url}getdict").json()
+    req=process_notes(preps,phone_dict)
+    phonemes=get_rhythm(req)
+    f0=get_f0(req)
+    model="1215_opencpop_ds1000_fix_label_nomidi"
+    submit_req={
+        "model": model,
+        "phonemes":phonemes,
+        "f0":{
+            "timestep":0.005,
+            "values":f0
+        },
+        "speedup": 50
+    }
+    
+    submit=httpx.request("POST",f"{url}submit", json=submit_req).json()
+    wav=None
+    while True:
+        time.sleep(1)
+        stat=httpx.request("POST",f'{url}query', json={"token":submit["token"]}).json()
+        if stat["status"]=="HIT_CACHE":
+            break
+        elif stat["status"] == "QUEUED":
+            continue
+        elif stat["status"] == "RUNNING":
+            continue
+        elif stat["status"] == "FINISHED":
+            break
+        elif stat["status"] == "FAILED":
+            await ds.finish("错误："+stat["message"])
+        elif stat["status"] == "CANCELLED":
+            await ds.finish("已取消")
+        
+    wav=httpx.request("GET",f"{url}download",params={"token":submit["token"]}).content
+    await ds.finish(MessageSegment.record(wav))
+    
+def get_rhythm(req):
+    resp=httpx.request("POST",url+'rhythm', json=req)
+    phonemes=resp.json()['phonemes']
+    return phonemes
---
+++ nonebot_plugin_diffsinger/data_source.py
@@ -0,0 +1,45 @@
+import librosa
+import httpx
+
+def process_notes(preps,phone_dict):
+    notes=[]
+    for prep in preps:
+        if prep[2] in phone_dict:
+            notes.append({"key": prep[0],"duration": prep[1],"slur": False,"phonemes": phone_dict[prep[2]]})
+        else:
+            if prep[2] in ["AP","SP"]:
+                notes.append({"key": prep[0],"duration": prep[1],"slur": False,"phonemes": [prep[2]]})
+            elif prep[2] == "+":
+                notes.append({"key": prep[0],"duration": prep[1],"slur": True})
+            else: 
+                notes.append({"key": prep[0],"duration": prep[1],"slur": False,"phonemes": [prep[2]]})
+    req={"notes":notes}
+    return req
+
+def bpm2dur(bpm=120,input=[]):
+    preps=[]
+    for note in input:
+        dur_bar=60*4/bpm
+        dur=(note[1]/16)*dur_bar
+        preps.append([note[0],dur,note[2]])
+    return preps
+
+def get_f0(input_notes,f0_timestep=0.005):
+    midi_note=[]
+    for note in input_notes['notes']:
+        midi_note.append(note['key'])
+    note_dur=[]
+    for noted in input_notes['notes']:
+        note_dur.append(noted['duration'])
+    f0=[]
+    for i in range(len(note_dur)):
+        notes=[]
+        for _ in range(int(note_dur[i]/f0_timestep)):
+            if midi_note[i]==0:
+                f0.append(296.0)
+            else:
+                notes.append(midi_note[i])
+        f0.extend(librosa.midi_to_hz(notes).tolist())
+    return f0
+
+
